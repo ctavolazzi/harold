763cf7b73bec92d466e9376ff840ce79842be312
Christopher Tavolazzi <ctavolazzi@gmail.com>
2022-06-27 03:06:28 -0700

    Create createElem.js

---
+++ src/Functions/Rendering/createElem.js
@@ -0,0 +1,4 @@
+function createElem (info, destination) {
+  //initialized an element containing the passed-in info at the destination
+  //example: create a button next to the main submit button with a timer that counts down till the button deactivates
+}
