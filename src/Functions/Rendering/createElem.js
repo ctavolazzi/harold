e49329222ab2a8c00f6f3209e2fb7e8de3d30df7
Christopher Tavolazzi <ctavolazzi@gmail.com>
2022-06-27 03:07:18 -0700

    Update createElem.js

--- src/Functions/Rendering/createElem.js
+++ src/Functions/Rendering/createElem.js
@@ -1,4 +1,6 @@
 function createElem (info, destination) {
   //initialized an element containing the passed-in info at the destination
   //example: create a button next to the main submit button with a timer that counts down till the button deactivates
+  //         create a new image at the target destination
+  //         add a new textbox at the top right of the screen
 }
