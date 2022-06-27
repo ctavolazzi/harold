9001d91a9589dce42087fbacc24e9bf4475a7c8c
Christopher Tavolazzi <ctavolazzi@gmail.com>
2022-06-27 03:04:28 -0700

    Create clearElem.js

---
+++ src/Functions/Rendering/clearElem.js
@@ -0,0 +1,6 @@
+function clearElem (elem) {
+  //clear's whatever is displayed in the passed-in element
+  //example: pass in a text box element and this function will clear whatever text is displayed
+}
+
+export default clearElem;
