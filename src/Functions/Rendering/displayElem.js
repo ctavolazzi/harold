2a40fa5c0f1414c71c95c2e6adab567ebf61d847
Christopher Tavolazzi <ctavolazzi@gmail.com>
2022-06-27 03:02:34 -0700

    Create displayElem.js

---
+++ src/Functions/Rendering/displayElem.js
@@ -0,0 +1,7 @@
+function displayElem (elem, destination) {
+  //appends the element to the destination on the page
+  //maybe has some additional logic
+  //if it gets too complicated, break it down into separate functions
+};
+
+export default displayElem;
